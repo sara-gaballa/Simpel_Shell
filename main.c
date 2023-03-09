ab9988b19ada803205d41a7c9d98626f160915eb
Sara Gaballa <96298154+sara-gaballa@users.noreply.github.com>
2023-03-09 02:28:36 +0200

    final push

--- main.c
+++ main.c
@@ -7,7 +7,19 @@
 #include <errno.h>
 #define  MAX_LENGTH 1024
 FILE *fptr; //public pointer to the text file
-void removingQoutes(char *string){
+void Handler(){
+    int wstat;
+    pid_t pid;
+    while (1) {
+        pid = wait3(&wstat, WNOHANG, NULL);
+        fptr = fopen("log.txt","a");
+        fprintf(fptr,"ZOMBIES TERMINATED , Return code: %d\n", wstat);
+        if (pid == 0 || pid == -1)
+            return;
+        fclose(fptr);
+    }
+}
+void removingQoutes(char *string){ //removing qoutes from the string
     int j = 0;
     char new_string[1024];
     for (int i = 0; string[i] != '\0' && string[i] != '\n' ; ++i) {
@@ -19,52 +31,47 @@ void removingQoutes(char *string){
     new_string[j] = '\0';
     strcpy(string, new_string);
 }
-void executeShellBultin(char **command, char *argument[]){  ////
-    if(!strcmp(*command,"cd")){
-        if (argument[0] == NULL) {
-//            chdir(getenv(""));
-            char path[1024];
-            printf("The current path: %s\n",getcwd(path,sizeof (path)));
-        }else if (!strcmp(argument[0], "~")) {
+void executeShellBultin(char **command, char *argument[]){//execution of build in functions
+    if(!strcmp(*command,"cd")){//for cd command in opening directories
+        if (argument[0] == NULL) {//returning to the home path if null is entered
             chdir(getenv("HOME"));
             char path[1024];
             printf("The current path: %s\n",getcwd(path,sizeof (path)));
-        } else if (!strcmp(argument[0], "..")) {
+        }else if (!strcmp(argument[0], "~")) {//returning to the home path
+            chdir(getenv("HOME"));
+            char path[1024];
+            printf("The current path: %s\n",getcwd(path,sizeof (path)));
+        } else if (!strcmp(argument[0], "..")) {//retuning two places backwards in directory location
             chdir("../..");
             char path[1024];
             printf("The current path: %s\n",getcwd(path,sizeof (path)));
         } else if (chdir(argument[0]) == -1) {
             printf("cd: %s: %s\n",argument[0], strerror(errno));
-        } else {
+        } else {//opening the path entered by the user
             chdir(argument[0]);
             char path[1024];
             printf("The current path: %s\n",getcwd(path,sizeof (path)));
         }
-    }else if(!strcmp(*command,"export")){
-        char *var,*value;
-        var = strtok(argument[0], "=");
-        var[strlen(var)] = '\0';
-        printf("%d    ", strlen(var));
+    }else if(!strcmp(*command,"export")){ //for export and evaluating the expression
+        char *var,*value;//pointers for saving variable and value
+        var = strtok(argument[0], "=");//separation at "="
+        var[strlen(var)] = '\0';//adding null char to the end of the string
         value = strtok(NULL, "\n");
         value[strlen(value)] = '\0';
-        // printf("%s  %s", var, value);
-        removingQoutes(value);
-        if (setenv(var, value, 1) != 0) {
+        removingQoutes(value);//removing the qoutes if available
+        if (setenv(var, value, 1) != 0) {//saving the value to the variable using setenv()
             perror("setenv error");
             exit(EXIT_FAILURE);
         }
-        printf("%s=%s\n", var, getenv(var));
-    }else if(!strcmp(*command,"echo")){
+    }else if(!strcmp(*command,"echo")){//for echo command
         char *arg = argument[0]; //'hello $x'
-        printf("%s   %d\n",argument[0], strlen(argument[0]));
         removingQoutes(arg);
-        printf("%s   %d\n",arg, strlen(arg));
         for (int i = 0; i < strlen(arg); ++i) {
             if(arg[i] == '$'){
                 int j = 0;
                 i++;
                 char *var;
-                while (arg[i] != '\0' && arg[i] != ' '){
+                while (arg[i] != '\0' && arg[i] != ' '){//extracting the variable from the entered string
                     var[j++] = arg[i++];
                 }
                 i--;
@@ -78,8 +85,8 @@ void executeShellBultin(char **command, char *argument[]){  ////
         printf("\n");
     }
 }
-void execute_command(char **args, int ground){
-    pid_t pid = fork();
+void execute_command(char **args, int ground){//executing commands
+    pid_t pid = fork();//forking the child processes
     if (pid == 0) { /* Child process */
         execvp(args[0], args);
         printf("Command not found\n");
@@ -88,7 +95,7 @@ void execute_command(char **args, int ground){
         int status;
         waitpid(pid, &status, 0);
         fptr = fopen("log.txt","a");
-        fprintf(fptr,"%s","Child is terminated\n");
+        fprintf(fptr,"%s%d%s","Child ",pid," is terminated\n");
         fclose(fptr);
         if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
             printf("Command returned non-zero exit status\n");
@@ -97,22 +104,18 @@ void execute_command(char **args, int ground){
         printf("Failed to fork process\n");
     }
 }
-void evaluate_expression(char input[], char **command, char *argument[], int *AR_NUM, int *built_in ){
+void Parse_Input(char input[], char **command, char *argument[], int *AR_NUM, int *built_in ){//parsing input entered by the user
     char *Parsed_Input;
     Parsed_Input = strtok(input, " ");
     *command = Parsed_Input;
     int len = strlen(*command);
     if(command[0][len - 1] == '\n') // removing extra \n
         command[0][len - 1] = '\0';
-
     if(!strcmp(*command,"cd") || !strcmp(*command,"export") || !strcmp(*command,"echo")) { //check for built-in commands
         *built_in = 1;
         argument[0] = strtok(NULL, "\n");
-        //argument[0][strlen(argument[0])] = '\n';
-        // printf("%d\n", strlen(argument[0])); //taking the single argument of the built-in commands
     }
-    else {
-        //TO DO export arguments and echo
+    else {//extracting the arguments
         int I_A = 0; //index of arguments
         while (Parsed_Input != NULL) {
             Parsed_Input = strtok(NULL, " ");
@@ -128,43 +131,28 @@ void evaluate_expression(char input[], char **command, char *argument[], int *AR
         argument[I_A] = NULL;
     }
 }
-void setup_environment(){
+void setup_environment(){//setting the working directory to the current directory
     char path[1024];
     chdir(getcwd(path,sizeof (path)));
 }
-void proc_exit(){
-    int wstat;
-    pid_t pid;
-    while (1) {
-        // printf("hehehe");
-        pid = wait3(&wstat, WNOHANG, NULL);
-        fptr = fopen("log.txt","a");
-        fprintf(fptr,"CHILD TERMINATED , Return code: %d\n", wstat);
-        if (pid == 0 || pid == -1)
-            return;
-        fclose(fptr);
-    }
-}
-int main(int argc, char *argv[])
+int main()
 {
     fptr = fopen("log.txt", "w");//erase the file from any previous content
     fclose(fptr);
-    signal (SIGCHLD, proc_exit);
+    signal(SIGCHLD, Handler);//setting signal handler
     setup_environment();
     while (1){
         char input[MAX_LENGTH];
         char *command;
         char *argument[10];
-        int Arr_NUM = 0;
-        int B_IN = 0;
+        int Arr_NUM = 0;//number of arguments
+        int B_IN = 0;//boolean to check if build in function or not
         printf("SHELL-> ");
         fgets(input, sizeof(input), stdin);
         if(!strcmp(input, "\n")){ //if the user enters an empty line
             continue;
         }
-        evaluate_expression(input, &command, argument, &Arr_NUM, &B_IN);//taking commands and arguments from the input
-        //printf( "%d %d\n", Arr_NUM, B_IN);
-        //printf( "%s\n", argument[0]);
+        Parse_Input(input, &command, argument, &Arr_NUM, &B_IN);//taking commands and arguments from the input
         if(!strcmp(command,"exit")){ //exit command to terminate the shell
             exit(0);
         }
@@ -172,18 +160,15 @@ int main(int argc, char *argv[])
             if (B_IN == 0) {
                 int ground = 0; //no & forground
                 char *args[Arr_NUM + 2];
-                args[0] = command;
-                args[Arr_NUM + 1] = NULL;
+                args[0] = command; //saving the command in the first element of the array
+                args[Arr_NUM + 1] = NULL;//terminating the args with null
                 for (int i = 0; i < Arr_NUM; ++i) {
                     if(argument[i][0] == '$'){
                         char*  tmp = strsep(&argument[0], "$");
                         char* var = strsep(&argument[0], " ");
-                        printf("%s\n", getenv(var));
-
                         char* parse = strtok(getenv(var), " ");
                         while (parse != NULL){
                             args[++i] = parse;
-                            printf("%s\n",args[i]);
                             parse = strtok(NULL, " ");
                         }
                         args[i + 1] = NULL;
@@ -199,10 +184,9 @@ int main(int argc, char *argv[])
                 }
                 execute_command(&args, ground); //args must terminate with null
             } else {
-                executeShellBultin(&command, argument);
+                executeShellBultin(&command, argument);//if the commad is from build in functions
             }
         }
     }
-
 }
 
